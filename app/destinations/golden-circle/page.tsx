424a712f786288cf91817c64f91583b031519183
Siddharth Jha <jhasiddharth495@gmail.com>
2026-09-03 11:23:10 +0530

    Fixed

--- app/destinations/golden-circle/page.tsx
+++ app/destinations/golden-circle/page.tsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
+
 import GoldenCircleContent from "@/components/destinations/goldencircle/GoldenCircleContent";
 import GoldenCircleHero from "@/components/destinations/goldencircle/GoldenCircleHero";
 import PopularDestinations from "@/components/destinations/northern-lights/PopularDestinations";
+
 export default function Page() {
   return (
-    <main>
-       <GoldenCircleHero />
-       <GoldenCircleContent />
-       <PopularDestinations />
-    </main>
+    <Suspense fallback={null}>
+      <main>
+        <GoldenCircleHero />
+
+        <GoldenCircleContent />
+
+        <PopularDestinations />
+      </main>
+    </Suspense>
   );
 }
